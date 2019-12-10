0248d3a1a9bdf8bfade610366665a0388a72c5ff
Phuong Thao Hoang <thaohanicka@seznam.cz>
2019-12-10 17:44:47 +0100

    Fixed Tournament without replacement

--- gatbx/tourwithoutrepl.m
+++ gatbx/tourwithoutrepl.m
@@ -15,46 +15,20 @@
 %                by calculating OldChrom(NewChrIx,:).
 
 function NewChrIx = tourwithoutrepl(FitnV, Nsel)
-%Check the input values
-K=10;
-if (length(FitnV)-Nsel*K < 0)
-    %error("Check Nsel and K in the Tournament selection without replacement.");
-end
 
+K=10;
 current_member = 1;
-pomFitnV = FitnV;
+pomFitnV = sort(FitnV);
 NewChrIx = zeros(Nsel,1);
 
 while (current_member <= Nsel)
     %Pick K individuals randomly without replacement
-    pomFitnV = pomFitnV
     picked = sort(pomFitnV(randperm(length(pomFitnV),K)));
     
-    %Compare these K individuals and select the best of them
-<<<<<<< HEAD
-    %best = 0;
-    %for ind=1:K
-    %    if picked(ind)>best
-    %        best = picked(ind);
-    %    end
-    %end
-    NewChrIx(current_member) = find(FitnV==picked(1,1),1);
+    NewChrIx(current_member) = find(FitnV==picked(1,1));
     current_member = current_member+1;
     
-    %throw out the already K selected members
+    %throw out the already selected best member
     pomFitnV = pomFitnV(pomFitnV~=picked(1,1));  
-=======
-    best = picked(1);
-    for ind=2:K
-        if picked(ind)<best
-            best = picked(ind);
-        end
-    end
-    NewChrIx(current_member) = find(FitnV==best,1);
-    current_member = current_member+1;
-    
-    %throw out the already K selected members
-    pomFitnV = pomFitnV(pomFitnV~=best);  
->>>>>>> dcaf58dfe850c4d51a6eb998e659c8508b0e1930
 end
 end
